c5134e31f2118d6c9040b7b13da15bd81951866a
Eliza Weisman <eliza@buoyant.io>
2018-05-30 10:33:17 -0700

    Fix test code

--- proxy/tests/support/controller.rs
+++ proxy/tests/support/controller.rs
@@ -181,6 +181,7 @@ pub fn destination_add_labeled(
                         }),
                         weight: 0,
                         metric_labels: addr_labels,
+                        tls_verification: None,
                     },
                 ],
                 metric_labels: set_labels,
